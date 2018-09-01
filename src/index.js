11d05032755cac19d86dcb7db55fe6e5c5789e49
Acadamia <42879753+Acadamia@users.noreply.github.com>
2018-09-01 07:14:42 -0400

    conception

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function O(i) {
+    return document.getElementById(i);
+}
+
+function settingsscreen () {
+    O('titlescreen').style.display = 'none';
+    O('settingsscreen').style.display = 'block';
+}
